16624ef2b1111c951209d086a4568fa6ee60590a
Danthewaann <danielcrblack@gmail.com>
2023-12-10 12:28:31 +0000

    refactor: replace logger with print statements

--- src/pyallel/main.py
+++ src/pyallel/main.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import os
 import sys
 import time
@@ -7,10 +6,6 @@ from datetime import timedelta
 from dataclasses import dataclass
 import importlib.metadata
 import subprocess
-
-logging.basicConfig(format="%(message)s")
-logging.StreamHandler.terminator = ""
-logger = logging.getLogger("pyallel")
 
 WHITE_BOLD = "\033[1m"
 GREEN_BOLD = "\033[1;32m"
@@ -91,14 +86,16 @@ def indent(output: str) -> str:
     return "\n".join("    " + line for line in output.splitlines())
 
 
-def main_loop(commands: list[str], fail_fast: bool = False) -> bool:
+def main_loop(
+    commands: list[str], fail_fast: bool = False, verbose: bool = False
+) -> bool:
     processes = run_commands(commands)
     completed_processes: set[str] = set()
     passed = True
 
     while True:
         for icon in ICONS:
-            logger.info(f"{CLEAR_LINE}\r{WHITE_BOLD}Running commands{NC} {icon}")
+            print(f"{CLEAR_LINE}\r{WHITE_BOLD}Running commands{NC} {icon}", end="")
             time.sleep(0.1)
 
         for process in processes:
@@ -108,29 +105,29 @@ def main_loop(commands: list[str], fail_fast: bool = False) -> bool:
             if process.process.poll() is None:
                 continue
 
-            logger.info(f"${CLEAR_LINE}\r")
+            print(f"${CLEAR_LINE}\r", end = "")
             elapsed = time.perf_counter() - process.start
             completed_processes.add(process.name)
             if process.process.returncode != 0:
                 passed = False
-                logger.info(f"{RED_BOLD}{process.name} ")
-                logger.debug(f"[{timedelta(seconds=elapsed)}] ")
-                logger.info(f": fail {X}{NC}\n")
+                print(f"{RED_BOLD}{process.name} ", end="")
+                print(f"[{timedelta(seconds=elapsed)}] ", end="")
+                print(f": fail {X}{NC}")
                 if process.process.stdout:
                     output = process.process.stdout.read()
                     if output:
-                        logger.info(f"{indent(output.decode())}\n")
+                        print(f"{indent(output.decode())}")
 
                 if fail_fast:
                     return False
             else:
-                logger.info(f"{GREEN_BOLD}{process.name} ")
-                logger.debug(f"[{timedelta(seconds=elapsed)}] ")
-                logger.info(f": pass {TICK}{NC}\n")
+                print(f"{GREEN_BOLD}{process.name} ", end="")
+                print(f"[{timedelta(seconds=elapsed)}] ", end="")
+                print(f": pass {TICK}{NC}")
                 if process.process.stdout:
                     output = process.process.stdout.read()
                     if output:
-                        logger.debug(f"{indent(output.decode())}\n")
+                        print(f"{indent(output.decode())}")
 
         if len(completed_processes) == len(processes):
             break
@@ -141,27 +138,21 @@ def main_loop(commands: list[str], fail_fast: bool = False) -> bool:
 def run() -> None:
     parser = create_parser()
     args = parser.parse_args(namespace=Arguments())
-
-    if args.verbose:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-
     if args.version:
         my_version = importlib.metadata.version("pyallel")
-        logger.info("%s\n", my_version)
+        print("%s\n", my_version)
         sys.exit(0)
 
     start_time = time.perf_counter()
 
     exit_code = 0
-    status = main_loop(args.commands, args.fail_fast)
+    status = main_loop(args.commands, args.fail_fast, args.verbose)
     if not status:
-        logger.info(f"\n{RED_BOLD}A command failed!{NC}\n")
+        print(f"\n{RED_BOLD}A command failed!{NC}")
         exit_code = 1
     else:
-        logger.info(f"\n{GREEN_BOLD}Success!{NC}\n")
+        print(f"\n{GREEN_BOLD}Success!{NC}")
 
     elapsed_time = time.perf_counter() - start_time
-    logger.debug(f"Time taken : {timedelta(seconds=elapsed_time)}\n")
+    print(f"Time taken : {timedelta(seconds=elapsed_time)}")
     sys.exit(exit_code)
