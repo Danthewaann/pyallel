246c59234e8280d353464b29a5b5f3406f9d3f93
Danthewaann <danielcrblack@gmail.com>
2023-12-29 14:30:33 +0000

    refactor: add prettify_error test util function

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,14 @@ import subprocess
 from pyallel import main
 from pytest import CaptureFixture
 
+from tests.utils import prettify_error
+
 
 def test_run_single_command(capsys: CaptureFixture[str]) -> None:
-    assert main.run("sleep 0.1") == 0
+    exit_code = main.run("sleep 0.1")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
     assert out == [
         "Running commands...\n",
         "\n",
@@ -16,10 +19,26 @@ def test_run_single_command(capsys: CaptureFixture[str]) -> None:
     ]
 
 
-def test_run_single_command_with_env(capsys: CaptureFixture[str]) -> None:
-    assert main.run("TEST_VAR=1 sleep 0.1") == 0
+def test_run_single_command_with_output(capsys: CaptureFixture[str]) -> None:
+    exit_code = main.run("echo 'hi'")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
+    assert out == [
+        "Running commands...\n",
+        "\n",
+        "[echo] done ✓\n",
+        "    hi\n",
+        "\n",
+        "Success!\n",
+    ]
+
+
+def test_run_single_command_with_env(capsys: CaptureFixture[str]) -> None:
+    exit_code = main.run("TEST_VAR=1 sleep 0.1")
+    captured = capsys.readouterr()
+    out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
     assert out == [
         "Running commands...\n",
         "\n",
@@ -30,9 +49,10 @@ def test_run_single_command_with_env(capsys: CaptureFixture[str]) -> None:
 
 
 def test_run_multiple_commands(capsys: CaptureFixture[str]) -> None:
-    assert main.run("sh -c 'sleep 0.1; echo \"first\"'", "echo 'hi'", "-s") == 0
+    exit_code = main.run("sh -c 'sleep 0.1; echo \"first\"'", "echo 'hi'", "-s")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
     assert out == [
         "Running commands...\n",
         "\n",
@@ -47,9 +67,10 @@ def test_run_multiple_commands(capsys: CaptureFixture[str]) -> None:
 
 
 def test_run_fail_fast(capsys: CaptureFixture[str]) -> None:
-    assert main.run("sleep 0.1", 'sh -c "exit 1"', "-f") == 1
+    exit_code = main.run("sleep 0.1", 'sh -c "exit 1"', "-f")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 1, prettify_error(captured.out)
     assert out == [
         "Running commands...\n",
         "\n",
@@ -60,9 +81,10 @@ def test_run_fail_fast(capsys: CaptureFixture[str]) -> None:
 
 
 def test_run_verbose_mode(capsys: CaptureFixture[str]) -> None:
-    assert main.run("sleep 0.1", "-V") == 0
+    exit_code = main.run("sleep 0.1", "-V")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
     assert out == [
         "Arguments:\n",
         "    commands    : ['sleep 0.1']\n",
@@ -82,9 +104,10 @@ def test_run_verbose_mode(capsys: CaptureFixture[str]) -> None:
 
 
 def test_run_debug_mode(capsys: CaptureFixture[str]) -> None:
-    assert main.run("sleep 0.1", "-d") == 0
+    exit_code = main.run("sleep 0.1", "-d")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 0, prettify_error(captured.out)
     assert out == [
         "Running commands...\n",
         "\n",
@@ -97,24 +120,28 @@ def test_run_debug_mode(capsys: CaptureFixture[str]) -> None:
 
 
 def test_handles_invalid_executable(capsys: CaptureFixture[str]) -> None:
-    assert main.run("invalid_exe") == 1
+    exit_code = main.run("invalid_exe")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 1, prettify_error(captured.out)
     assert out == [
         "Error: executables [invalid_exe] were not found\n",
     ]
 
 
 def test_handles_many_invalid_executables(capsys: CaptureFixture[str]) -> None:
-    assert main.run("invalid_exe", "other_invalid_exe") == 1
+    exit_code = main.run("invalid_exe", "other_invalid_exe")
     captured = capsys.readouterr()
     out = captured.out.splitlines(keepends=True)
+    assert exit_code == 1, prettify_error(captured.out)
     assert out == [
         "Error: executables [invalid_exe, other_invalid_exe] were not found\n",
     ]
 
 
-def test_does_not_run_executables_on_parsing_error() -> None:
-    assert main.run("invalid_exe", "other_invalid_exe", "sleep 10") == 1
+def test_does_not_run_executables_on_parsing_error(capsys: CaptureFixture[str]) -> None:
+    exit_code = main.run("invalid_exe", "other_invalid_exe", "sleep 10")
+    captured = capsys.readouterr()
+    assert exit_code == 1, prettify_error(captured.out)
     status = subprocess.run(["pgrep", "sleep"])
     assert status.returncode == 1, "sleep shouldn't be running!"
---
+++ tests/utils.py
@@ -0,0 +1,2 @@
+def prettify_error(out: str) -> str:
+    return f"Got an error\n\n{out}"
