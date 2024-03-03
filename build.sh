df8c3c36304601e8495f9e4158d4cc6fd12206b1
Danthewaann <danielcrblack@gmail.com>
2024-03-03 09:51:19 +0000

    build: add flag to ignore signals in the pyinstaller bootloader

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 distro="${1:-linux}"
 
-# Exclude PyInstaller from built packages
+# --bootloader-ignore-signals is needed as two interrupt signals where getting sent
+# to pyallel as it is apart of the same process group as the bootloader
+# From: https://pyinstaller.org/en/stable/usage.html#cmdoption-bootloader-ignore-signals
 pyinstaller \
     --onefile \
     --noconfirm \
@@ -10,5 +12,6 @@ pyinstaller \
     --log-level DEBUG \
     --exclude-module PyInstaller \
     --copy-metadata pyallel \
+    --bootloader-ignore-signals \
     --name pyallel-"$(pyallel -v)"-"$distro"-x86_64 \
     ./src/pyallel/main.py
