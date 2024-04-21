5f214258a784e1e26216eb29c0d458250dda0e92
Danthewaann <danielcrblack@gmail.com>
2024-04-21 14:55:17 +0100

    build: use pyallel to build pyallel

--- build_all.sh
+++ build_all.sh
@@ -2,33 +2,11 @@
 
 rm -rf build dist specs
 
-# Generic linux
-echo "### BUILDING GENERIC LINUX - x86_64 ###"
-echo
-docker build --tag pyallel --build-arg 'arch=x86_64' --build-arg "uid=$(id -u)" --build-arg "gid=$(id -g)" . && \
-    docker run -e 'arch=x86_64' --rm --volume "$(pwd):/src" pyallel
-echo
-echo "### BUILDING GENERIC LINUX - aarch64 ###"
-echo
-docker build --tag pyallel --build-arg 'arch=aarch64' --build-arg "uid=$(id -u)" --build-arg "gid=$(id -g)" . && \
-    docker run -e 'arch=aarch64' --rm --volume "$(pwd):/src" pyallel
-echo
-echo "### DONE GENERIC LINUX ###"
+user_id=$(id -u)
+group_id=$(id -g)
+cur_dir=$(pwd)
 
-# Alpine linux
-echo "### BUILDING ALPINE LINUX - x86_64 ###"
-echo
-docker build --tag pyallel-alpine --build-arg 'arch=x86_64' --build-arg "uid=$(id -u)" --build-arg "gid=$(id -g)" --file Dockerfile.alpine . && \
-    docker run -e 'arch=x86_64' --rm --volume "$(pwd):/src" pyallel-alpine
-echo
-echo "### BUILDING ALPINE LINUX - aarch64 ###"
-echo
-docker build --tag pyallel-alpine --build-arg 'arch=aarch64' --build-arg "uid=$(id -u)" --build-arg "gid=$(id -g)" --file Dockerfile.alpine . && \
-    docker run -e 'arch=aarch64' --rm --volume "$(pwd):/src" pyallel-alpine
-echo
-echo "### DONE ALPINE LINUX ###"
-
-# Local OS
+# Local OS setup
 distro=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
 
@@ -40,11 +18,11 @@ else
     arch=unknown
 fi
 
-echo "### BUILDING LOCAL ###"
-echo
-python -m venv .venv && \
-    . .venv/bin/activate && \
-    pip install . -r requirements_build.txt && \
-    ./build.sh "$distro" "$arch"
-echo
-echo "### DONE LOCAL ###"
+poetry install
+
+poetry run pyallel "docker build --tag pyallel-x86_64 --build-arg 'arch=x86_64' --build-arg 'uid=$user_id' --build-arg 'gid=$group_id' . && docker run -e 'arch=x86_64' --rm --volume '$cur_dir:/src' pyallel-x86_64" \
+        "docker build --tag pyallel-aarch64 --build-arg 'arch=aarch64' --build-arg 'uid=$user_id' --build-arg 'gid=$group_id' . && docker run -e 'arch=aarch64' --rm --volume '$cur_dir:/src' pyallel-aarch64" \
+        "docker build --tag pyallel-x86_64-alpine --build-arg 'arch=x86_64' --build-arg 'uid=$user_id' --build-arg 'gid=$group_id' --file Dockerfile.alpine . && docker run -e 'arch=x86_64' --rm --volume '$cur_dir:/src' pyallel-alpine-x86_64" \
+        "docker build --tag pyallel-aarch64-alpine --build-arg 'arch=aarch64' --build-arg 'uid=$user_id' --build-arg 'gid=$group_id' --file Dockerfile.alpine . && docker run -e 'arch=aarch64' --rm --volume '$cur_dir:/src' pyallel-alpine-aarch64" \
+        ::: \
+        "poetry run ./build.sh $distro $arch"
